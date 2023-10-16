080fe962c63b0cfbbf87e540a3652c5772bf6a65
Nison Zhang <zhang_shi_zhao@163.com>
2023-10-16 15:18:37 +0800

    feat: 轮播图交互

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -19,6 +19,18 @@ const getGoodsData = async () => {
 onLoad(() => {
   getGoodsData()
 })
+
+const curPage = ref(0)
+const onChange: UniHelper.SwiperOnChange = (ev) => {
+  curPage.value = ev.detail.current
+}
+
+const onTapImage = (url: string) => {
+  uni.previewImage({
+    current: url,
+    urls: goods.value!.mainPictures,
+  })
+}
 </script>
 
 <template>
@@ -27,15 +39,15 @@ onLoad(() => {
     <view class="goods">
       <!-- 商品主图 -->
       <view class="preview">
-        <swiper circular>
+        <swiper @change="onChange" circular>
           <swiper-item v-for="item in goods?.mainPictures" :key="item">
-            <image mode="aspectFill" :src="item" />
+            <image @tap="onTapImage(item)" mode="aspectFill" :src="item" />
           </swiper-item>
         </swiper>
         <view class="indicator">
-          <text class="current">1</text>
+          <text class="current">{{ curPage + 1 }}</text>
           <text class="split">/</text>
-          <text class="total">5</text>
+          <text class="total">{{ goods?.mainPictures.length }}</text>
         </view>
       </view>
 
