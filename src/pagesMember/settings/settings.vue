177ee81168c8c0c9e20869a61c0c9aeaba1c9746
Nison Zhang <zhang_shi_zhao@163.com>
2023-10-16 22:59:42 +0800

    feat: 个人中心-退出登录

--- src/pagesMember/settings/settings.vue
+++ src/pagesMember/settings/settings.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
-//
+import { useMemberStore } from '@/stores'
+
+const memberStore = useMemberStore()
+const onLogout = () => {
+  uni.showModal({
+    content: '是否退出登录？',
+    success: (res) => {
+      if (res.confirm) {
+        memberStore.clearProfile()
+        uni.navigateBack()
+      }
+    },
+  })
+}
 </script>
 
 <template>
   <view class="viewport">
     <!-- 列表1 -->
-    <view class="list" v-if="true">
+    <view class="list" v-if="memberStore.profile">
       <navigator url="/pagesMember/address/address" hover-class="none" class="item arrow">
         我的收货地址
       </navigator>
@@ -21,7 +34,7 @@
       <navigator hover-class="none" class="item arrow" url=" ">关于小兔鲜儿</navigator>
     </view>
     <!-- 操作按钮 -->
-    <view class="action">
+    <view @tap="onLogout" class="action" v-if="memberStore.profile">
       <view class="button">退出登录</view>
     </view>
   </view>
