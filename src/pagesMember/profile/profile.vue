865abd88f4f43e9e635ca700426a198f636b9277
Nison Zhang <zhang_shi_zhao@163.com>
2023-10-29 15:17:37 +0800

    feat: 会员中心-个人信息头像更新

--- src/pagesMember/profile/profile.vue
+++ src/pagesMember/profile/profile.vue
@@ -12,6 +12,34 @@ const getMemberProfileData = async () => {
   memberProfile.value = res.result
 }
 
+// 修改头像
+const onAvatarChange = () => {
+  // 调用拍照/选择照片
+  uni.chooseMedia({
+    count: 1,
+    mediaType: ['image'],
+    success: (res) => {
+      // 本地路径
+      const { tempFilePath } = res.tempFiles[0]
+      // 文件上传
+      uni.uploadFile({
+        url: '/member/profile/avatar',
+        name: 'file',
+        filePath: tempFilePath,
+        success: (res) => {
+          if (res.statusCode === 200) {
+            const avatar = JSON.parse(res.data).result.avatar
+            memberProfile.value!.avatar = avatar
+            uni.showToast({ icon: 'success', title: '更新头像成功' })
+          } else {
+            uni.showToast({ icon: 'error', title: '出现错误' })
+          }
+        },
+      })
+    },
+  })
+}
+
 onLoad(() => {
   getMemberProfileData()
 })
@@ -26,7 +54,7 @@ onLoad(() => {
     </view>
     <!-- 头像 -->
     <view class="avatar">
-      <view class="avatar-content">
+      <view @tap="onAvatarChange" class="avatar-content">
         <image class="image" :src="memberProfile?.avatar" mode="aspectFill" />
         <text class="text">点击修改头像</text>
       </view>
