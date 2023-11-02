bc28bf36e0a42162b4ba6cd9b71d67efd6cd7961
Nison Zhang <zhang_shi_zhao@163.com>
2023-11-02 16:05:29 +0800

    feat: 填写订单-提交订单

--- src/pagesOrder/create/create.vue
+++ src/pagesOrder/create/create.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
 import { onLoad } from '@dcloudio/uni-app'
 import { computed, ref } from 'vue'
-import { getMemberOrderPreAPI, getMemberOrderPreNowAPI } from '@/services/order'
+import { getMemberOrderPreAPI, getMemberOrderPreNowAPI, postMemberOrderAPI } from '@/services/order'
 import type { OrderPreResult } from '@/types/order'
-import type { AddressItem } from '@/types/address'
 import { useAddressStore } from '@/stores/modules/address'
 
 // 获取屏幕边界到安全区域距离
@@ -49,6 +48,21 @@ const addressStore = useAddressStore()
 const selectAddress = computed(() => {
   return addressStore.selectedAddress || orderPre.value?.userAddresses.find((v) => v.isDefault)
 })
+
+const onOrderSubmit = async () => {
+  if (!selectAddress.value?.id) {
+    return uni.showToast({ icon: 'none', title: '请选择收货地址' })
+  }
+  const res = await postMemberOrderAPI({
+    addressId: selectAddress.value.id,
+    buyerMessage: buyerMessage.value,
+    deliveryTimeType: activeDelivery.value.type,
+    goods: orderPre.value!.goods.map((v) => ({ count: v.count, skuId: v.skuId })),
+    payChannel: 2,
+    payType: 1,
+  })
+  uni.redirectTo({ url: `/pagesOrder/detail/detail?id=${res.result.id}` })
+}
 
 onLoad(() => {
   getMemberOrderPreData()
@@ -137,7 +151,9 @@ onLoad(() => {
     <view class="total-pay symbol">
       <text class="number">{{ orderPre?.summary.totalPayPrice.toFixed(2) }}</text>
     </view>
-    <view class="button" :class="{ disabled: true }"> 提交订单 </view>
+    <view class="button" :class="{ disabled: !selectAddress?.id }" @tap="onOrderSubmit">
+      提交订单
+    </view>
   </view>
 </template>
 
--- src/services/order.ts
+++ src/services/order.ts
@@ -1,4 +1,4 @@
-import type { OrderPreResult } from '@/types/order'
+import type { OrderCreateParams, OrderPreResult } from '@/types/order'
 import { http } from '@/utils/http'
 
 export const getMemberOrderPreAPI = () => {
@@ -16,6 +16,19 @@ export const getMemberOrderPreNowAPI = (data: {
   return http<OrderPreResult>({
     method: 'GET',
     url: '/member/order/pre/now',
+    data,
+  })
+}
+
+/**
+ * 提交订单
+ * @param data 请求参数
+ * @returns
+ */
+export const postMemberOrderAPI = (data: OrderCreateParams) => {
+  return http<{ id: string }>({
+    method: 'POST',
+    url: '/member/order',
     data,
   })
 }
